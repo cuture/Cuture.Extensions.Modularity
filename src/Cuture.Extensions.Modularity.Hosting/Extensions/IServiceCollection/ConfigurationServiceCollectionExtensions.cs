783c1dfbfb58da5da44c99127e9b3e6532152264
Stratos <StratosBlue@outlook.com>
2022-08-05 09:13:19 +0800

    调整GetConfiguration命名空间

--- src/Cuture.Extensions.Modularity.Hosting/Extensions/IServiceCollection/ConfigurationServiceCollectionExtensions.cs
+++ src/Cuture.Extensions.Modularity.Hosting/Extensions/IServiceCollection/ConfigurationServiceCollectionExtensions.cs
@@ -3,68 +3,68 @@
 using Cuture.Extensions.Modularity.Internal;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Cuture.Extensions.Modularity;
+
+/// <summary>
+///
+/// </summary>
+public static class ConfigurationServiceCollectionExtensions
 {
+    #region Public 方法
+
     /// <summary>
-    ///
+    /// 从<see cref="IServiceCollection"/>中获取<see cref="IConfiguration"/>
     /// </summary>
-    public static class ConfigurationServiceCollectionExtensions
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IConfiguration? GetConfiguration(this IServiceCollection services)
     {
-        #region Public 方法
-
-        /// <summary>
-        /// 从<see cref="IServiceCollection"/>中获取<see cref="IConfiguration"/>
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IConfiguration? GetConfiguration(this IServiceCollection services)
+        if (services.TryGetObjectAccessorValue<IConfigurationContainer>(out var iConfigurationContainer)
+            && iConfigurationContainer is not null)
         {
-            if (services.TryGetObjectAccessorValue<IConfigurationContainer>(out var iConfigurationContainer)
-                && iConfigurationContainer is not null)
-            {
-                return iConfigurationContainer.Value;
-            }
-            var hostBuilderContext = services.GetSingletonServiceInstance<HostBuilderContext>();
-
-            return hostBuilderContext?.Configuration as IConfigurationRoot
-                   ?? services.GetSingletonServiceInstance<IConfiguration>()
-                   ?? (services.TryGetObjectAccessorValue<IConfiguration>(out var configuration) ? configuration : null);
+            return iConfigurationContainer.Value;
         }
+        var hostBuilderContext = services.GetSingletonServiceInstance<HostBuilderContext>();
 
-        /// <inheritdoc cref="GetConfiguration(IServiceCollection)"/>
-        public static IConfiguration GetRequiredConfiguration(this IServiceCollection services) => services.GetConfiguration()
-                                                                                                   ?? throw new InvalidOperationException($"Not found {nameof(IConfiguration)} in serviceCollection.");
+        return hostBuilderContext?.Configuration as IConfigurationRoot
+               ?? services.GetSingletonServiceInstance<IConfiguration>()
+               ?? (services.TryGetObjectAccessorValue<IConfiguration>(out var configuration) ? configuration : null);
+    }
 
-        /// <summary>
-        /// 将用于<see cref="GetConfiguration(IServiceCollection)"/>获取的<see cref="IConfiguration"/>添加到<paramref name="services"/>中
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="configuration"></param>
-        /// <returns>是否为新添加</returns>
-        public static bool SetConfiguration(this IServiceCollection services, IConfiguration configuration)
+    /// <inheritdoc cref="GetConfiguration(IServiceCollection)"/>
+    public static IConfiguration GetRequiredConfiguration(this IServiceCollection services) => services.GetConfiguration()
+                                                                                               ?? throw new InvalidOperationException($"Not found {nameof(IConfiguration)} in serviceCollection.");
+
+    /// <summary>
+    /// 将用于<see cref="GetConfiguration(IServiceCollection)"/>获取的<see cref="IConfiguration"/>添加到<paramref name="services"/>中
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns>是否为新添加</returns>
+    public static bool SetConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        if (services.TryGetObjectAccessor<IConfigurationContainer>(out var container)
+            && container is not null)
         {
-            if (services.TryGetObjectAccessor<IConfigurationContainer>(out var container)
-                && container is not null)
+            if (container.Value is null)
             {
-                if (container.Value is null)
-                {
-                    container.Value = new(configuration);
-                }
-                else
-                {
-                    container.Value.Value = configuration;
-                }
-                return false;
+                container.Value = new(configuration);
             }
             else
             {
-                services.AddObjectAccessor<IConfigurationContainer>(new(configuration));
-                return true;
+                container.Value.Value = configuration;
             }
+            return false;
         }
-
-        #endregion Public 方法
+        else
+        {
+            services.AddObjectAccessor<IConfigurationContainer>(new(configuration));
+            return true;
+        }
     }
+
+    #endregion Public 方法
 }
--- src/Cuture.Extensions.Modularity.Hosting/Extensions/IServiceCollection/HostBuilderServiceCollectionExtensions.cs
+++ src/Cuture.Extensions.Modularity.Hosting/Extensions/IServiceCollection/HostBuilderServiceCollectionExtensions.cs
@@ -2,28 +2,56 @@
 
 using Cuture.Extensions.Modularity.Internal;
 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Cuture.Extensions.Modularity;
+
+/// <summary>
+///
+/// </summary>
+public static class HostBuilderServiceCollectionExtensions
 {
+    #region Public 方法
+
     /// <summary>
-    ///
+    /// 从<see cref="IServiceCollection"/>中获取<see cref="IHostBuilder"/>
     /// </summary>
-    public static class HostBuilderServiceCollectionExtensions
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IHostBuilder? GetHostBuilder(this IServiceCollection services) => services.GetObjectAccessorValue<IHostBuilderContainer>()?.Value;
+
+    /// <inheritdoc cref="GetHostBuilder(IServiceCollection)"/>
+    public static IHostBuilder GetRequiredHostBuilder(this IServiceCollection services) => services.GetHostBuilder()
+                                                                                           ?? throw new InvalidOperationException($"Not found {nameof(IHostBuilder)} in serviceCollection.");
+
+    /// <summary>
+    /// 将用于<see cref="GetHostBuilder(IServiceCollection)"/>获取的<see cref="IHostBuilder"/>添加到<paramref name="services"/>中
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="hostBuilder"></param>
+    /// <returns>是否为新添加</returns>
+    public static bool SetHostBuilder(this IServiceCollection services, IHostBuilder hostBuilder)
     {
-        #region Public 方法
-
-        /// <summary>
-        /// 从<see cref="IServiceCollection"/>中获取<see cref="IHostBuilder"/>
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IHostBuilder? GetHostBuilder(this IServiceCollection services) => services.GetObjectAccessorValue<IHostBuilderContainer>()?.Value;
-
-        /// <inheritdoc cref="GetHostBuilder(IServiceCollection)"/>
-        public static IHostBuilder GetRequiredHostBuilder(this IServiceCollection services) => services.GetHostBuilder()
-                                                                                               ?? throw new InvalidOperationException($"Not found {nameof(IHostBuilder)} in serviceCollection.");
-
-        #endregion Public 方法
+        if (services.TryGetObjectAccessor<IHostBuilderContainer>(out var container)
+            && container is not null)
+        {
+            if (container.Value is null)
+            {
+                container.Value = new(hostBuilder);
+            }
+            else
+            {
+                container.Value.Value = hostBuilder;
+            }
+            return false;
+        }
+        else
+        {
+            services.AddObjectAccessor<IHostBuilderContainer>(new(hostBuilder));
+            return true;
+        }
     }
+
+    #endregion Public 方法
 }
